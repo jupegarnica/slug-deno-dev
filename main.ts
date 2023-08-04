ede162280cdaa96eef2b84332dc06c5338a2e0a7
Jupegarnica <juan@geekshubs.com>
2023-08-04 12:39:12 +0200

    🎨 style(main.ts): update form HTML structure and add CSS styles for better readability and user experience 🐛 fix(main.ts): handle missing slug and url in POST request and return appropriate error response ✨ feat(main.ts): display success message with short link when it is created and redirect to the original URL when accessing the short link

--- main.ts
+++ main.ts
@@ -1,8 +1,48 @@
 const kv = await Deno.openKv();
 
-const form = `
+const html = `
+  <style>
+    input {
+      font-size: 1.4rem;
+      padding: 0.5rem;
+      border: 0;
+      border-bottom: 1px solid #ccc;
+      min-width: 300px;
+      max-width: 600px;
+    }
+    form {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      gap: 1rem;
+    }
+    * {
+      font-family: monospace;
+
+    }
+    body {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      min-height: 100vh;
+    }
+    .error {
+      background: tomato;
+    }
+    .success {
+      background: limegreen;
+    }
+    h2 {
+      color: white;
+      padding: 0.5rem 1em;
+    }
+  </style>
   <form action="/" method="POST">
     <input name="slug" placeholder="slug" />
+    <input name="url" placeholder="url" />
+    <input type="submit" value="SAVE" />
   </form>
 `
 
@@ -11,10 +51,34 @@ Deno.serve(async (request: Request) => {
 
   // Create short links
   if (request.method == "POST") {
-    const body = await request.text();
-    const { slug, url } = JSON.parse(body);
+
+    const body = await request.formData();
+    const slug = body.get("slug") as string;
+    const url = body.get("url") as string;
+    if (!slug || !url) {
+      const m = html + `<h2 class="error">Error: slug and url are required</h2>`;
+      console.log(`Error: slug and url are required`);
+
+      return new Response(m, {
+        status: 400,
+        headers: { "content-type": "text/html" },
+      });
+    }
     const result = await kv.set(["links", slug], url);
-    return new Response(JSON.stringify(result));
+    if (result.ok) {
+      const m = html + `<h2 class="success">Short link created: <a href="/${slug}">/${slug}</a></h2>`;
+      console.log(`Short link created: /${slug}`);
+
+      return new Response(m, {
+        headers: { "content-type": "text/html" },
+      });
+    } else {
+      const m = html + `<h2 class="error">Error: ${result}</h2>`;
+      console.log(`Error: ${result}`);
+
+      return new Response(m,
+        { status: 404, headers: { "content-type": "text/html" } });
+    }
   }
 
   // Redirect short links
@@ -23,9 +87,10 @@ Deno.serve(async (request: Request) => {
   const slug = request.url.split("/").pop() || "";
   const url = (await kv.get(["links", slug])).value as string;
   if (url) {
+    console.log(`Redirecting to ${url}`);
     return Response.redirect(url, 301);
   } else {
-    const m = !slug ? form : `Slug "${slug}" not found`;
+    const m = !slug ? html : html + `<h2 class="error">Slug "${slug}" not found</h2>`;
     return new Response(m, {
       status: 404,
       headers: { "content-type": "text/html" },
